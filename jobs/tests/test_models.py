76a00d8c0c0d0ac7b1534a7c7464257bab798372
Edgar <me@edgarlatorre.com.br>
2015-09-18 17:19:18 +0100

    created method to return description without html tags

--- jobs/tests/test_models.py
+++ jobs/tests/test_models.py
@@ -35,6 +35,10 @@ class TestJob(TestCase):
         secondjob = mommy.make(Job, token = '12345')
         self.assertNotEqual(firstjob.token, secondjob.token)
 
+    def test_get_clean_description(self):
+        self.job.description = "<p>&nbsp;test</p>"
+        self.assertEqual(self.job.get_clean_description(), "&nbsp;test")
+
     def job_is_valid(self, job):
         is_valid = True
         try :
