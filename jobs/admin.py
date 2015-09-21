d30c492feca975dff57fa925e72d101e1b1a7f4a
Edgar <me@edgarlatorre.com.br>
2015-09-22 00:14:48 +0100

    Some changes to adapt the code to pep8

--- jobs/admin.py
+++ jobs/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from .models import Job
 
+
 @admin.register(Job)
 class JobAdmin(admin.ModelAdmin):
     pass
--- pythonjobs/services.py
+++ pythonjobs/services.py
@@ -1,19 +1,21 @@
-import string, random
+import string
+import random
 from django.core.mail import send_mail
 from django.shortcuts import render
 from django.template import loader
 
 
-def generate_token(length = 60):
+def generate_token(length=60):
     chars = string.ascii_uppercase + string.digits
     return ''.join(random.choice(chars) for _ in range(length))
+
 
 def send_confirmation_mail(job):
     subject = "You just posted a job on pythonjobs.ie"
     template = loader.get_template("email.html")
-    body = template.render({'job' : job})
-    to_email = [ job.email ]
+    body = template.render({'job': job})
+    to_email = [job.email]
     from_email = "info@kimera.io"
 
     return send_mail(subject, body, from_email, to_email,
-                fail_silently = True, html_message = body)
+                fail_silently=True, html_message=body)
