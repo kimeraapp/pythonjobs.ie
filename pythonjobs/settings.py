f513860689204eaf9d364e8fbb67918db209b6f0
Edgar <me@edgarlatorre.com.br>
2015-09-22 22:40:30 +0100

    Added celery settings

--- pythonjobs/settings.py
+++ pythonjobs/settings.py
@@ -111,3 +111,10 @@ EMAIL_HOST_USER = os.environ.get('EMAIL_USER')
 EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_PASSWORD')
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
+
+
+# Celery
+
+BROKER_URL = os.environ.get('BROKER_URL')
+CELERY_RESULT_BACKEND = os.environ.get('BROKER_URL')
+CELERY_ACCEPT_CONTENT = ['json']
