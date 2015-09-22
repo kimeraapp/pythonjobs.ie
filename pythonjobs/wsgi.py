b1f0c8c748e7b4470419408acef3f62463227aa6
Edgar <me@edgarlatorre.com.br>
2015-09-22 17:51:29 +0100

    importing django noise after set variable

--- pythonjobs/wsgi.py
+++ pythonjobs/wsgi.py
@@ -10,9 +10,11 @@ https://docs.djangoproject.com/en/1.8/howto/deployment/wsgi/
 import os
 
 from django.core.wsgi import get_wsgi_application
-from whitenoise.django import DjangoWhiteNoise
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "pythonjobs.settings")
 
 application = get_wsgi_application()
+
+from whitenoise.django import DjangoWhiteNoise
+
 application = DjangoWhiteNoise(application)
