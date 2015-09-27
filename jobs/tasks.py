cd65ac2d0437729689436a8e0ed50a2af650cc0e
Edgar <me@edgarlatorre.com.br>
2015-09-27 17:48:18 +0100

    changed tweet message

--- jobs/tasks.py
+++ jobs/tasks.py
@@ -3,7 +3,8 @@ from pythonjobs.services import Twitter
 
 
 @app.task
-def tweet(job):
-    message = "New python job on {0}".format(job.get_absolute_url())
+def tweet(job_url):
+    print("Sharing message on twitter")
+    message = u"Check the new job on http://pythonjobs.ie{0}".format(job_url)
     twitter = Twitter()
     twitter.tweet(message)
