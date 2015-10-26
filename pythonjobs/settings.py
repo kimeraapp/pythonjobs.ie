953a2b22669132971924fc07c235de6590c342ba
Jorge Barata <jorge.barata.gonzalez@gmail.com>
2015-10-26 10:24:28 +0000

    Turn off DEBUG by default

--- pythonjobs/settings.py
+++ pythonjobs/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 SECRET_KEY = os.environ.get('SECRET_KEY', 'change-it')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get('DEBUG', False)
 
 ALLOWED_HOSTS = []
 
