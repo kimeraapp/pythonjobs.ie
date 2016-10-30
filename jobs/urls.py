27dba123edf2e22d5f40c7995662a6e3759b6777
Edgar <me@edgarlatorre.com.br>
2016-10-30 12:56:56 +0000

    Create robots.txt template and url

--- jobs/urls.py
+++ jobs/urls.py
@@ -1,4 +1,5 @@
 from django.conf.urls import url
+from django.views.generic import TemplateView
 from jobs.views import IndexView, ShowView, NewView, EditView, JobsFeed
 
 urlpatterns = [
@@ -8,4 +9,7 @@ urlpatterns = [
         EditView.as_view(slug_field="token"), name='job-edit'),
     url(r'^$', IndexView.as_view(), name='job-home'),
     url(r'^rss/$', JobsFeed()),
+    url(r'^robots\.txt$',
+        TemplateView.as_view(template_name='robots.txt', content_type='text/plain'),
+        name="robots"),
 ]
